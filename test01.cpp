6e4e3eefcfd2f872475952f45b8499d82c1dea3a
wuyubo <wuyubo_berton@163.com>
2016-08-21 11:16:30 +0800

    a c++ inherit test

---
+++ test01.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+	A()
+	{
+		cout<<"setup A"<<endl;
+	};
+	virtual ~A()
+	{
+		cout<<"release A"<<endl;
+	};
+	void last()
+	{
+		cout<<"killed last"<<endl;
+	}
+	virtual void killed()
+	{
+		cout<<"killed A"<<endl;
+		delete this;
+		cout<<"killed this"<<endl;
+		last();
+	}
+};
+
+class B: public A
+{
+public:
+	B()
+	{
+		cout<<"setup B"<<endl;
+	};
+	virtual ~B()
+	{
+		cout<<"release B"<<endl;
+	};
+	
+};
+
+int main()
+{
+	B *b = new B;
+	A *a = b;
+	a->killed();
+	while(1);
+	return 0;
+}
